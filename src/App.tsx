4c325b038c1ca05644825418061544084a73ee43
marcelozair <marceloberrocal2004@gmail.com>
2022-09-26 22:59:18 -0500

    Tailwindcss installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { SignIn } from './views/auth/SignIn/SignIn';
 
 function App() {
   return (
     <main className="app">
       <Routes>
-        <Route path="/auth/sign-in" element={<p>sign in</p>} />
+        <Route path="/auth/sign-in" element={<SignIn />} />
         <Route path="/auth/sign-up" element={<p>sign up</p>} />
         <Route path="/" element={<p>none</p>} />
       </Routes>
---
+++ src/views/auth/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import './SignIn.css'
+
+export const SignIn = () => {
+  return (
+    <main className="signin">
+      <p>SignIn</p>
+    </main>
+  )
+}
